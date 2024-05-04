89f1ef2af6763c79c871daeaa414ecdcdb98d94b
brianwaw <bwaweru408@gmail.com>
2024-05-04 19:13:22 +0300

    updated views

--- base/views.py
+++ base/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 
-
 # Create your views here.
 
 rooms = [
-    {'id':1, 'name':'Lets learn Python'},
-    {'id':2, 'name':'Lets learn Golang'},
-    {'id':3, 'name':'Lets learn C++'},
+    {'id': 1, 'name': 'Lets learn Python'},
+    {'id': 2, 'name': 'Lets learn Golang'},
+    {'id': 3, 'name': 'Lets learn C++'},
 ]
 
 
 def home(request):
-    context = {'rooms':rooms}
+    context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
+
 
 def room(request, pk):
     room = None
@@ -20,6 +20,6 @@ def room(request, pk):
         if i['id'] == pk:
             room = i
 
-    context = {'room':room}
+    context = {'room': room}
 
     return render(request, 'base/room.html', context)
